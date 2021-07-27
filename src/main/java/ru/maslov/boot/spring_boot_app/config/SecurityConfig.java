3dafd7931c2ff1a23ad375ceff8804f4fd190fb4
Charik635 <cuber1319@gmail.com>
2021-07-27 16:53:25 +0300

    Ready!

---
+++ src/main/java/ru/maslov/boot/spring_boot_app/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+
+package ru.maslov.boot.spring_boot_app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import ru.maslov.boot.spring_boot_app.config.handler.LoginSuccessHandler;
+import ru.maslov.boot.spring_boot_app.service.UserServiceIml;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private UserServiceIml userServiceIml;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userServiceIml;
+    }
+    private  UserDetailsService userDetailsService;// сервис, с помощью которого тащим пользователя
+    private  LoginSuccessHandler successUserHandler;// класс, в котором описана логика перенаправления пользователей по ролям
+
+
+    public SecurityConfig(@Qualifier("userServiceIml") UserDetailsService userDetailsService,
+                          LoginSuccessHandler successUserHandler) {
+        this.userDetailsService = userDetailsService;
+        this.successUserHandler   = successUserHandler;
+    }
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()); // конфигурация для прохождения аутентификации
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                // указываем страницу с формой логина
+                //указываем логику обработки при логине
+                .successHandler(new LoginSuccessHandler())
+                // даем доступ к форме логина всем
+                .permitAll();
+
+        http.logout()
+                // разрешаем делать логаут всем
+                .permitAll()
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login")
+                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
+                .and().csrf().disable();
+
+        http
+                // делаем страницу регистрации недоступной для авторизированных пользователей
+                .authorizeRequests()
+                //страницы аутентификаци доступна всем
+                .antMatchers("/login").anonymous()
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                // защищенные URL
+                .antMatchers("/user/**").access("hasAnyRole('USER', 'ADMIN')")
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
+
+
